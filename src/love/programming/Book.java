e2428ca42ddca62b81e1e9b384ae3e8fb0869308
jyothipaul@yahoo.com <jyothipaul@yahoo.com>
2019-03-17 21:39:26 +0000

    javaconstructionPractice

---
+++ src/love/programming/Book.java
@@ -0,0 +1,40 @@
+package love.programming;
+
+import java.sql.SQLOutput;
+
+public class Book {
+
+    String bookGenre;
+    String bookName;
+    int noOfPages;
+
+    public Book() {
+//        bookGenre = "fiction";
+//        bookName = "geek girl";
+//        noOfPages = 100;
+//
+//        System.out.println("Without args: " + bookGenre);
+//        System.out.println("Without args: " + bookName);
+//        System.out.println("Without args: " + noOfPages);
+    }
+
+    public Book(String bookGenre) {
+        System.out.println("With 1 args: " + bookGenre);
+    }
+
+    public Book(String bookGenre, String bookName) {
+        System.out.println("With 2 args: " + bookGenre);
+        System.out.println("With 2 args: " + bookName);
+    }
+
+    public Book(String bookGenre, String bookName, int noOfPages) {
+        System.out.println("With 3 args: " + bookGenre);
+        System.out.println("With 3 args: " + bookName);
+        System.out.println("With 3 args: " + noOfPages);
+
+    }
+}
+
+
+
+
---
+++ src/love/programming/MyMainClass.java
@@ -0,0 +1,11 @@
+package love.programming;
+
+public class MyMainClass {
+
+    public static void main(String[] args) {
+        Book bookObject1 = new Book();
+        Book bookObject2 = new Book("Non-Fiction");
+        Book bookObject3 = new Book("Sci-Fiction","Sci Boy");
+        Book bookObject4 = new Book("Fiction","head over heals" ,250);
+    }
+}
